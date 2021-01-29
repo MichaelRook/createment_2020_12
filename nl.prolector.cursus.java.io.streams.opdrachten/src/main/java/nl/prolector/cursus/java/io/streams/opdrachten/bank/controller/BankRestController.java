222ba782ae5074d23d02936af30cfde230a570f5
Roel Huijskens <roelh@hotmail.com>
2021-01-29 12:10:35 +0100

    small update webservices

--- nl.prolector.cursus.java.io.streams.opdrachten/src/main/java/nl/prolector/cursus/java/io/streams/opdrachten/bank/controller/BankRestController.java
+++ nl.prolector.cursus.java.io.streams.opdrachten/src/main/java/nl/prolector/cursus/java/io/streams/opdrachten/bank/controller/BankRestController.java
@@ -1,10 +1,15 @@
 package nl.prolector.cursus.java.io.streams.opdrachten.bank.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.prolector.cursus.java.io.streams.opdrachten.bank.entity.BankDAO;
 import nl.prolector.cursus.java.io.streams.opdrachten.bank.entity.BankEntity;
+import nl.prolector.cursus.java.io.streams.opdrachten.bank.entity.SqlDao;
+import nl.prolector.cursus.java.io.streams.opdrachten.bank.toa.BankEntity2BankWS;
+import nl.prolector.cursus.java.io.streams.opdrachten.bank.webservices.Bank;
 
 @RestController
 public class BankRestController {
@@ -14,12 +19,29 @@ public class BankRestController {
 	
 	
 
-    @RequestMapping(value = "/api/bank", method = RequestMethod.GET)
-    public BankEntity showall() {
+    @RequestMapping(value = "/api/bank/", method = RequestMethod.GET)
+    public Bank showall() {
 		BankEntity bank = new BankEntity("My Bank");
 		bank.openRekeningCourant("piet", 10.0);
-		return bank;
+		BankEntity2BankWS toaBank =  new BankEntity2BankWS();
+		Bank annotatedBank = toaBank.convert(bank);
+		return annotatedBank;
     }
+    
+    
+    @RequestMapping(value = "/api/addbank/{banknaam}/{houder}", method = RequestMethod.GET)
+    public boolean addBank(@PathVariable("houder") String houder, @PathVariable("banknaam") String banknaam) {
+		boolean isAdded = false;
+    	BankEntity bank = new BankEntity(banknaam);
+		bank.openRekeningCourant(houder, 10.0);
+		BankDAO aDao = new SqlDao();
+		isAdded = aDao.add(bank);
+		
+		return isAdded;
+    }
+    
+    
+    
     
 //    @Transactional
 //    @RequestMapping(value = "/api/Klant/maarten", method = RequestMethod.GET)
